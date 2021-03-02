92c49e1799ecaf98096ccbd165902629b979230c
Patricio <patodotaop@hotmail.com>
2021-03-02 17:26:45 -0300

    Little Random Changes

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,8 +3,8 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto Funciona de maravilla perri");
+	alert("Esto Funciona de maravilla perrisssssss");
 
-
+	alert("Say hey hey hey");
 }
 
